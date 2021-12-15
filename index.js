e3003791422f344ee3bbee09e1a9c37e556052da
Anderson <you@example.com>
2021-12-15 20:58:34 -0300

    subindo images

---
+++ index.js
@@ -0,0 +1,15 @@
+
+export const images = {
+
+    //Logos
+    logoCirculo : require('./logoCirculo.png'),
+    logoTijuca: require('./logoTijuca.png'),
+    mundoTijuca: require('./mundoTijuca.png'),
+
+    //Produtos
+    frangoCongelado: require('./frangoCongelado.jpg'),
+    queijoCoalho: require('./queijoCoalho.jpg'),
+    bandejaOvos: require('./bandeja.png'),
+    bandejaFrango: require('./sobrecoxa.png')
+
+}
